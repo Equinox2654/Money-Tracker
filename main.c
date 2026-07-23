3c5da1f23bc2591184a5a90915a53a9e82ba9a13
Equinox2654 <thibishaan@gmail.com>
2026-07-23 14:49:33 +1000

    Added the ability to add an item to a budget

--- main.c
+++ main.c
@@ -9,8 +9,10 @@ int main(int argc, char *argv[]) {
     char balance_text[17];
     double balance_amount = 0.0;
 
-    balance = fopen("balance.txt", "a");
-    fclose(balance);
+    FILE *items;
+    items = fopen("items.txt", "a"); fclose(items);
+
+    balance = fopen("balance.txt", "a"); fclose(balance);
 
     balance_file = fopen("balance.txt", "r");
     fgets(balance_text, 17, balance_file);
@@ -19,8 +21,9 @@ int main(int argc, char *argv[]) {
     balance_amount = strtod(balance_text, NULL);
     if (argc == 1) printf("Balance: %.2f\n", balance_amount);
 
+    log = fopen("log.txt", "a");
+
     if (argc == 3) {
-	log = fopen("log.txt", "a");
 	if (strcmp(argv[1], "add") == 0) {
 	    double amount_to_add = strtod(argv[2], NULL);
 	    balance_amount += amount_to_add;
@@ -31,8 +34,26 @@ int main(int argc, char *argv[]) {
 	    balance_amount -= amount_to_reduce;
 	    fprintf(log, "Spent: %.2f\n", amount_to_reduce);
 	}
-	fclose(log);
     }
+
+    if (argc == 5) {
+	if (strcmp(argv[1], "budget") == 0) {
+	    items = fopen("items.txt", "a");
+	    if (strcmp(argv[2], "add") == 0) {
+		char *item = argv[3];
+		int cost = strtod(argv[4], NULL);
+		if (!cost) {
+		    perror("Invalid pricing for item");
+		    return 1;
+		}
+		fprintf(items, "%s: %d\n", item, cost);
+		printf("%s: %d\n", item, cost);
+	    }
+	    fclose(items);
+	}
+    }
+
+    fclose(log);
 
     if (strcmp(argv[1], "help") == 0) {
 	printf("Tracks money:\nAdd: command adds money\nSpend: command spends money\n\nYou have $%.2f left.\n", balance_amount);
